9b9985bf6260fd289b3c9aa43acb07d3b5d5e1a3
palukurupradeep <149184923+palukurupradeep@users.noreply.github.com>
2024-09-30 16:54:30 +0530

    Create version.tf

---
+++ KorTerraform/version.tf
@@ -0,0 +1,18 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.0"
+      #version = "~> 2.0"             
+      #version = ">= 2.0.0, < 2.60.0"
+      #version = ">= 2.0.0, <= 2.64.0"   
+      #version = "~> 2.64" # For Production grade              
+    }
+  }
+}
+# Provider Block
+provider "azurerm" {
+features {}
+}
